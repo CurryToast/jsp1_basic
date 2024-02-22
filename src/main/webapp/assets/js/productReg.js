b391f2208ec37c126821e40b1f909f66adc1d6a1
CurryToast <gusqjacjswo39@gmail.com>
2024-02-22 17:50:06 +0900

    라이브러리 추가

---
+++ src/main/webapp/assets/js/productReg.js
@@ -0,0 +1,8 @@
+document.getElementById("proreg").addEventListener('click', () => {
+	let isValid = true;
+	// const inputs = document.querySelectorAll('form > div > input');
+	
+	if (isValid) {
+		document.forms[0].submit();
+	}
+});
